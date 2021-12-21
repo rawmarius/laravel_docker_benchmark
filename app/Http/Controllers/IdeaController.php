e04949af20b34557e8967f1d138419689cabce51
Mario Palomera <palomera.mario@gmail.com>
2021-12-21 16:22:07 -0500

    PHp 8.1 and public endpoint

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -15,7 +15,7 @@ class IdeaController extends Controller
      */
     public function index()
     {
-        return view('idea.index');
+        return Idea::paginate();
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IdeaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Public Endpoints for ideas
+Route::get('/ideas', [IdeaController::class, 'index']);
+Route::get('/ideas/{idea:slug}', [IdeaController::class, 'show']);
